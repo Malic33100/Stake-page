2f918fbe80e9f96daccf09e191b49e21c8b540bd
Malik <mertus.malik@gmail.com>
2022-06-14 14:41:48 -0400

    added pit pics

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ toggles.forEach(toggle => {
         toggle.parentNode.classList.toggle('active')
     })
 });
+
+const imgs = document.getElementById("imgs");
+/* image carousel identifiers */
+const img = document.querySelectorAll('#imgs img');
+
+/* toggle faq buttons*/
+let idx = 0;
+let interval = setInterval(run, 4000);
+
+function run() {
+    idx++;
+    changeImage()
+}
+
+function changeImage() {
+if(idx > img.length - 1){
+    idx = 0
+}else if(idx < 0){
+    idx = img.length - 1;
+}
+imgs.style.transform = `translateX(${-idx * 250}px)`;
+}
+
+function resetInterval () {
+clearInterval();
+interval = setInterval(run, 4000);
+}
